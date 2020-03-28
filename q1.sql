9fc4b4cbd56dd0de8e2d3e13590e111056149836
shaoyezh <yezheng.shao@mail.utoronto.ca>
2020-03-28 15:55:39 -0400

    a3

--- q1.sql
+++ q1.sql
@@ -16,28 +16,66 @@ CREATE TABLE q1 (
 );
 
 drop view if EXISTS SiteMonitorCapacity CASCADE;
+drop view if EXISTS SiteMonitorCapacity CASCADE;
+drop view if EXISTS SiteProvideOpen;
+drop view if EXISTS SiteProvideCave;
+drop view if EXISTS SiteProvideDeep;
 
+-- find site and divetype that has monitor to supervise
 create view SiteMonitorCapacity as 
-select * from sitemonitor, monitorcapacity
-where name = monitor;
+select site, divetype from sitemonitor, monitorcapacity
+where name = monitor and capacity != 0;
 
-create view openwater as 
-select count(distinct site) as openwater 
-from SiteMonitorCapacity
-where capacity != 0 and divetype = 'open water';
+-- find site that has a monitor that supervise open water
+create view SiteMonitorOpenWater as
+select distinct site from SiteMonitorCapacity 
+where divetype = 'open dive';
 
-create view cavedive as 
-select count(distinct site) as cave 
-from SiteMonitorCapacity
-where  capacity!= 0 and divetype = 'cave dive';
+-- find site that has a monitor that supervise cave dive
+create view SiteMonitorCave as 
+select distinct site from SiteMonitorCapacity 
+where divetype = 'cave dive';
 
-create view deepwater as 
-select count(distinct site) as deepwater
-from SiteMonitorCapacity
-where capacity != 0 and divetype = 'deep water';
+-- find site that has a monitor that supervise deep water
+create view SiteMonitorDeep as 
+select distinct site from SiteMonitorCapacity 
+where divetype = 'deep water';
+
+-- find site provide open water
+create view SiteProvideOpen as 
+select name from sitedivetype where divetype = 'open water';
+
+-- find site provide cave dive
+create view SiteProvideCave as 
+select name from sitedivetype where divetype = 'cave dive';
+
+-- find site provide deep water
+create view SiteProvideDeep as 
+select name from sitedivetype where divetype = 'deep water';
+
+-- count number of site that provide openwater 
+-- and has monitor supervise it.
+create view OpenWater as 
+select count(*) as openwater
+from SiteProvideOpen provide join SiteMonitorOpenWater monitor 
+on name = site;
+
+-- count number of site that provide cavedive 
+-- and has monitor supervise it.
+create view CaveDive as 
+select count(*) as cavedive
+from SiteProvideCave provide join SiteMonitorCave monitor 
+on name = site;
+
+-- count number of site that provide deepwater 
+-- and has monitor supervise it.
+create view DeepWater as 
+select count(*) as DeepWater
+from SiteProvideDeep provide join SiteMonitorDeep monitor 
+on name = site;
 
 Insert into q1
-select * from openwater, cavedive, deepwater;
+select * from OpenWater, CaveDive, DeepWater;
 
 
 
